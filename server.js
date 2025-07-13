d9adf8e2c363fffce3e66bfe6ed368a7e9a002ed
Nazrin Azizli <nazizli1112@gmail.com>
2025-07-13 13:23:13 +0400

    Update server.js

--- server.js
+++ server.js
@@ -1,30 +1,11 @@
 // server.js
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 5000;
-
-server.use(middlewares);
-
-// Custom route handler for /movies
-server.get('/movies', (req, res) => {
-  const db = router.db; // lowdb instance
-  const movies = db.get('movies').value();
-
-  const generateRandomId = () => Math.random().toString(36).substr(2, 9);
-
-  const moviesWithIds = movies.map(movie => ({
-    id: generateRandomId(),
-    ...movie
-  }));
-
-  res.jsonp(moviesWithIds);
-});
-
-// All other routes
-server.use(router);
-
-server.listen(port, () => {
-  console.log(`✅ JSON Server is running at http://localhost:${port}`);
-});
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+ 
+server.use(middlewares)
+server.use('', router)
+server.listen(process.env.PORT || 5000, () => {
+  console.log('JSON Server is running')
+})
