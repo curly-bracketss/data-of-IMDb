9a684297408848487a080c7c9874edc9215a66e5
Nazrin Azizli <nazizli1112@gmail.com>
2025-07-13 13:15:52 +0400

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,31 @@
 // server.js
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
- 
-server.use(middlewares)
-server.use('', router)
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+
+// Middleware to inject random IDs into each movie
+const generateRandomId = () => Math.random().toString(36).substr(2, 9);
+
+server.use((req, res, next) => {
+  if (req.method === 'GET' && req.url === '/movies') {
+    const db = router.db; // lowdb instance
+    const movies = db.get('movies').value();
+
+    const moviesWithIds = movies.map(movie => ({
+      id: generateRandomId(),
+      ...movie
+    }));
+
+    res.jsonp(moviesWithIds);
+  } else {
+    next(); // Let the default router handle everything else
+  }
+});
+
+server.use('', router);
 server.listen(process.env.PORT || 5000, () => {
-  console.log('JSON Server is running')
-})
+  console.log('JSON Server is running');
+});
